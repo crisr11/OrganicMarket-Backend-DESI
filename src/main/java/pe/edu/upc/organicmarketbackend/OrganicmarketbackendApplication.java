60703aa49e34499c1404b92ac584a888e081b192
antony <antonygc2077@gmail.com>
2022-10-24 12:48:31 -0500

    MayoristaCRUD & Search

--- src/main/java/pe/edu/upc/organicmarketbackend/OrganicmarketbackendApplication.java
+++ src/main/java/pe/edu/upc/organicmarketbackend/OrganicmarketbackendApplication.java
@@ -2,8 +2,10 @@ package pe.edu.upc.organicmarketbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan("pe.edu.upc.organicmarketbackend")
 public class OrganicmarketbackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/pe/edu/upc/organicmarketbackend/repositories/IMayoristaRepository.java
+++ src/main/java/pe/edu/upc/organicmarketbackend/repositories/IMayoristaRepository.java
@@ -1,10 +1,16 @@
 package pe.edu.upc.organicmarketbackend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.organicmarketbackend.entities.Mayorista;
+
+import java.util.List;
 
 
 @Repository
 public interface IMayoristaRepository extends JpaRepository<Mayorista,Integer> {
+    @Query("SELECT m FROM Mayorista m WHERE lower(m.nameMayorista) like lower(concat('%',:nameMayorista,'%'))")
+    List<Mayorista> searchByName(@Param("nameMayorista") String nameMayorista);
 }
