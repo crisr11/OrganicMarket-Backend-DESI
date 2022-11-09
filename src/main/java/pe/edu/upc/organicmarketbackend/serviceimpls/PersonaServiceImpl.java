e1636bcb87c650debe86f7f238608f2265a9211a
Luigui Mejía Mendoza <50814418+HortsB@users.noreply.github.com>
2022-11-09 11:34:41 -0500

    Actualizacion de serviceimpls: Persona, administrador

--- src/main/java/pe/edu/upc/organicmarketbackend/serviceimpls/PersonaServiceImpl.java
+++ src/main/java/pe/edu/upc/organicmarketbackend/serviceimpls/PersonaServiceImpl.java
@@ -7,6 +7,7 @@ import pe.edu.upc.organicmarketbackend.repositories.IPersonaRepository;
 import pe.edu.upc.organicmarketbackend.serviceinterfaces.IPersonaService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonaServiceImpl implements IPersonaService {
@@ -29,6 +30,11 @@ public class PersonaServiceImpl implements IPersonaService {
 
     @Override
     public List<Persona> search(String nombrePersona) {
-        return pR.buscarNombre(nombrePersona);
+        return pR.buscar(nombrePersona);
+    }
+
+    @Override
+    public Optional<Persona> listarId(int idPersona) {
+        return pR.findById(idPersona);
     }
 }
