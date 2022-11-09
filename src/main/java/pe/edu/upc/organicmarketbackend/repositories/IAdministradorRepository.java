f2c05f80b88eae9f0cb910fdbff6f482721ddc95
Luigui Mejía Mendoza <50814418+HortsB@users.noreply.github.com>
2022-11-09 11:33:54 -0500

    Actualizacion de repositorios: Persona, administrador

--- src/main/java/pe/edu/upc/organicmarketbackend/repositories/IAdministradorRepository.java
+++ src/main/java/pe/edu/upc/organicmarketbackend/repositories/IAdministradorRepository.java
@@ -1,8 +1,15 @@
 package pe.edu.upc.organicmarketbackend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.organicmarketbackend.entities.Administrador;
+
+import java.util.List;
+
 @Repository
 public interface IAdministradorRepository extends JpaRepository<Administrador,Integer> {
+    @Query("from Administrador a where a.persona.nombrePersona like %:nombrePersona%")
+    List<Administrador> buscarPersona(@Param("nombrePersona") String nombrePersona);
 }
