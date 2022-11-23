e2dae268cb828772cc7ee31959d6fc28ce395f6d
Luigui Mejía Mendoza <50814418+HortsB@users.noreply.github.com>
2022-11-22 20:52:15 -0500

    Implementacion de Queries

--- src/main/java/pe/edu/upc/organicmarketbackend/repositories/IPersonaRepository.java
+++ src/main/java/pe/edu/upc/organicmarketbackend/repositories/IPersonaRepository.java
@@ -12,4 +12,10 @@ import java.util.List;
 public interface IPersonaRepository extends JpaRepository<Persona,Integer> {
     @Query("from Persona p where p.nombrePersona like %:nombrePersona%")
     List<Persona>buscar(@Param("nombrePersona")String nombrePersona);
+    @Query(value = "SELECT p.dni_persona,p.nombre_persona,p.correo_persona from persona p ORDER by p.dni_persona asc;",nativeQuery = true)
+    List<String[]>buscarDniOrdenASC();
+    @Query(value = "SELECT p.dni_persona,p.nombre_persona,p.correo_persona FROM persona p ORDER BY p.dni_persona desc;",nativeQuery = true)
+    List<String[]>buscarDniOrdenDESC();
+    @Query(value = "SELECT p.nombre_persona,a.direccion,o.fecha_compra FROM persona p INNER JOIN agricultor a ON a.id_persona= p.id_persona INNER JOIN orden o ON o.id_agricultor= a.id_agricultor WHERE o.fecha_compra BETWEEN '2022-01-01' AND CURRENT_DATE;",nativeQuery = true)
+    List<String[]>buscarCompras();
 }
