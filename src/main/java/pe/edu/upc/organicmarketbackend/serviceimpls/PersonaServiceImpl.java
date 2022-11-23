776b405cc68eb4720cc0ab0b842f4458915e17e8
Luigui Mejía Mendoza <50814418+HortsB@users.noreply.github.com>
2022-11-22 20:51:55 -0500

    Implementacion de Queries

--- src/main/java/pe/edu/upc/organicmarketbackend/serviceimpls/PersonaServiceImpl.java
+++ src/main/java/pe/edu/upc/organicmarketbackend/serviceimpls/PersonaServiceImpl.java
@@ -3,9 +3,13 @@ package pe.edu.upc.organicmarketbackend.serviceimpls;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.edu.upc.organicmarketbackend.entities.Persona;
+import pe.edu.upc.organicmarketbackend.entities.ReporteAgriOrden;
+import pe.edu.upc.organicmarketbackend.entities.ReportePersona1;
+import pe.edu.upc.organicmarketbackend.entities.ReportePersona2;
 import pe.edu.upc.organicmarketbackend.repositories.IPersonaRepository;
 import pe.edu.upc.organicmarketbackend.serviceinterfaces.IPersonaService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,6 +40,45 @@ public class PersonaServiceImpl implements IPersonaService {
     @Override
     public Optional<Persona> listarId(int idPersona) {
         return pR.findById(idPersona);
+    }
+
+    @Override
+    public List<ReportePersona1> buscarDniOrdenASC() {
+        List<ReportePersona1>lista=new ArrayList<>();
+        pR.buscarDniOrdenASC().forEach(data->{
+            ReportePersona1 rp1=new ReportePersona1();
+            rp1.setDniPersona(data[0]);
+            rp1.setNombrePersona(data[1]);
+            rp1.setCorreoPersona(data[2]);
+            lista.add(rp1);
+        });
+        return lista;
+    }
+
+    @Override
+    public List<ReportePersona2> buscarDniOrdenDESC() {
+        List<ReportePersona2>lista=new ArrayList<>();
+        pR.buscarDniOrdenDESC().forEach(data->{
+            ReportePersona2 rp2=new ReportePersona2();
+            rp2.setDniPersona(data[0]);
+            rp2.setNombrePersona(data[1]);
+            rp2.setCorreoPersona(data[2]);
+            lista.add(rp2);
+        });
+        return lista;
+    }
+
+    @Override
+    public List<ReporteAgriOrden> buscarCompras() {
+        List<ReporteAgriOrden>lista=new ArrayList<>();
+        pR.buscarCompras().forEach(data->{
+            ReporteAgriOrden rao=new ReporteAgriOrden();
+            rao.setNombrePersona(data[0]);
+            rao.setDireccionAgricultor(data[1]);
+            rao.setFechaCompra(data[2]);
+            lista.add(rao);
+        });
+        return lista;
     }
 }
 
