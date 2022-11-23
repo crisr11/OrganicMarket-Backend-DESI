0ec9a3e981ea6856d9de74f938c26c53f0216572
Luigui Mejía Mendoza <50814418+HortsB@users.noreply.github.com>
2022-11-22 20:50:56 -0500

    Implementacion de Queries

--- src/main/java/pe/edu/upc/organicmarketbackend/serviceinterfaces/IPersonaService.java
+++ src/main/java/pe/edu/upc/organicmarketbackend/serviceinterfaces/IPersonaService.java
@@ -1,6 +1,9 @@
 package pe.edu.upc.organicmarketbackend.serviceinterfaces;
 
 import pe.edu.upc.organicmarketbackend.entities.Persona;
+import pe.edu.upc.organicmarketbackend.entities.ReporteAgriOrden;
+import pe.edu.upc.organicmarketbackend.entities.ReportePersona1;
+import pe.edu.upc.organicmarketbackend.entities.ReportePersona2;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +14,8 @@ public interface IPersonaService {
     public void delete (int idPersona);
     List<Persona>search(String nombrePersona);
     public Optional<Persona>listarId(int idPersona);
+    List<ReportePersona1>buscarDniOrdenASC();
+    List<ReportePersona2>buscarDniOrdenDESC();
+    List<ReporteAgriOrden>buscarCompras();
+
 }
